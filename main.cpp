cc69c31087bf0aa1a9113608f994ce9d0412e807
qazaqyanke <beis23@inbox.ru>
2021-10-24 13:00:21 +0600

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double a, b, c;
+    int sum, multi;
+    double avarege;
+    cout << "input three numbers ";
+    cin >> a >> b >> c;
+    sum = a + b + c;
+    multi = a*b*c;
+    avarege = (a+b+c)/3;
+
+    cout << sum << endl;
+    cout << multi << endl;
+    cout << avarege << endl;
+
+
+}
+
